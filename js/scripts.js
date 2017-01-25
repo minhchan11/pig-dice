f76a6c1a1a3a530267e0eb105d2a99684894ea07
Minh Phuong and said ahmed <minh@>
2017-01-25 11:39:31 -0800

    add font for dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 //Defining global variables
-var roll = 0
-var score = 0
+var roll1 = 0
+var score1 = 0
+
+var roll2 = 0
+var score2 = 0
 
 //Business Logic
 function PigDice(currentRoll, totalRoll) {
@@ -10,39 +13,57 @@ function PigDice(currentRoll, totalRoll) {
 
 PigDice.prototype.totalScore = function() {
   if (this.totalRoll === 0) {
-    $(".diceOne").toggle();
+    $(".diceTwo,.diceOne").toggle();
+  } else if (this.totalRoll > 99) {
+    alert("You win!");
   }
 };
 
-function generate() {
-  roll = Math.floor(Math.random()*6) + 1
-  if (roll === 1) {
-    score = 0;
-  } else { score = score +=roll ;}
+function generate1() {
+  roll1 = Math.floor(Math.random()*6) + 1
+  if (roll1 === 1) {
+    score1 = 0;
+  } else { score1 = score1+=roll1 ;}
 };
 
-
+function generate2() {
+  roll2 = Math.floor(Math.random()*6) + 1
+  if (roll2 === 1) {
+    score2 = 0;
+  } else { score2 = score2+=roll2 ;}
+};
 
 //User Logic
 $(document).ready(function() {
 
   $("#player1Roll").last().click(function() {
-    generate();
-    $(".numberOne").text(roll);
-    $(".totalOne").text(score);
-    var rollOne = roll;
-    var scoreOne = score;
+    generate1();
+    $(".numberOne").text(roll1)
+    $("#numberOne").text(roll1);
+    $(".totalOne").text(score1);
+    var rollOne = roll1;
+    var scoreOne = score1;
     var playerOne = new PigDice (rollOne, scoreOne);
     console.log(playerOne);
     playerOne.totalScore();
 
-  })
+  });
 
-  $(".playerOne").submit(function(event) {
+  $("#player2Roll").last().click(function() {
+    generate2();
+    $(".numberTwo").text(roll2);
+    $("#numberTwo").text(roll2);
+    $(".totalTwo").text(score2);
+    var rollTwo = roll2;
+    var scoreTwo = score2;
+    var playerTwo = new PigDice (rollTwo, scoreTwo);
+    playerTwo.totalScore();
+
+  });
+
+  $("form").submit(function(event) {
     event.preventDefault();
-    $(".diceOne").toggle();
-
-
+    $(".diceOne,.diceTwo").toggle();
   });
 
 });
